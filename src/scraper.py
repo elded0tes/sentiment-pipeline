d1f46409d28a97a831eec9630fe5018cde6ef758
Erick Diaz N <erick.dneri@alumnos.udg.mx>
2026-04-15 17:25:05 -0600

    Funcion de webdriver

--- src/scraper.py
+++ src/scraper.py
@@ -18,4 +18,24 @@ import os
 load_dotenv()
 
 def init_driver():
+    #Driver en modo headless
+    options = webdriver.ChromeOptions()
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('user-agent=Mozilla/5.0')
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+        )
+        return driver
+
+def login_glassdoor(driver):
+    
+    
+                        
+        
+    
     
